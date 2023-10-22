f9e3b8316b402340704c5dbe815496c511109667
fathf3 <fatih-348@hotmail.com>
2023-10-22 04:00:17 +0300

    a

---
+++ Core/WebApi.Application/Features/Products/Exceptions/ProductTitleMustNotBeSameException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.Bases;
+
+namespace WebApi.Application.Features.Products.Exceptions
+{
+    public class ProductTitleMustNotBeSameException : BaseExceptions 
+    {
+        public ProductTitleMustNotBeSameException() : base("Aynı ürün mevcut")
+        {
+            
+        }
+        public ProductTitleMustNotBeSameException(string message) : base(message + " isimli başka ürün mevcut")
+        {
+
+        }
+    }
+}
